fb942768bdada107d5384f46dd0ba6cbf27fc91f
Marco Graf <33481763+grafmar@users.noreply.github.com>
2023-10-18 21:53:31 +0200

    Cleanup / optimization

--- src/Calculator_Toy/KeypadReader.cpp
+++ src/Calculator_Toy/KeypadReader.cpp
@@ -2,16 +2,16 @@
 #include <Keypad.h>
 
 // Keypad
-const byte ROWS = 4; //four rows
-const byte COLS = 3; //three columns
-char keys[ROWS][COLS] = {
+static const byte ROWS = 4; //four rows
+static const byte COLS = 3; //three columns
+static const char keys[ROWS][COLS] = {
   {'1','2','3'},
   {'4','5','6'},
   {'7','8','9'},
   {'<','0','>'}
 };
-byte rowPins[ROWS] = {10, 9, 2, 4}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {8, 7, 6}; //connect to the column pinouts of the keypad
+static const byte rowPins[ROWS] = {10, 9, 2, 4}; //connect to the row pinouts of the keypad
+static const byte colPins[COLS] = {8, 7, 6}; //connect to the column pinouts of the keypad
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
